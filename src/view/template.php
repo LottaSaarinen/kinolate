db209b9d3e91d566440597146eee21eaf85c693d
Lotta Saarinen <p95070@edu.sasky.fi>
2024-06-07 14:39:49 +0300

    Sivupohjat lisätty

---
+++ src/view/template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fi">
+  <head>
+    <title>KinoLate - <?=$this->e($title)?></title>
+    <meta charset="UTF-8">    
+  </head>
+  <body>
+    <header>
+      <h1>📹KinoLate</h1>
+    </header>
+    <section>
+      <?=$this->section('content')?>
+    </section>
+    <footer>
+      <hr>
+      <div> Kurpitsa Solutions</div>
+    </footer>
+  </body>
+</html>
